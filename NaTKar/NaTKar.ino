0d36748e4e0b1fbad2868a0957de7839a075b784
thetoothless <ninaling@ucla.edu>
2016-05-20 00:46:13 -0700

    connected left pcb, calibrated high/low values

--- NaTKar/NaTKar.ino
+++ NaTKar/NaTKar.ino
@@ -1,4 +1,5 @@
-#define PCB_SIGNAL 14
+#define PCB_SIGNAL_R 14
+#define PCB_SIGNAL_L 15
 #define PWM1 3
 #define PWM2 4
 #define IN1 5
@@ -7,6 +8,9 @@
 #define IN4 8
 
 int sum = 0;
+
+boolean LHigh(int val){ return val<0; }
+boolean RHigh(int val){ return val<-20; }
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,14 +21,14 @@ void setup() {
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
-  pinMode(PCB_SIGNAL, INPUT);
+  pinMode(PCB_SIGNAL_L, INPUT);
+  pinMode(PCB_SIGNAL_R, INPUT);
   
   int numOfSamples = 1000;
-  for(int i = 0; i < numOfSamples; i++){
-    int val = analogRead(PCB_SIGNAL);
-    sum+=val;
-  }
-  sum /= numOfSamples;
+  for(int i = 0; i < numOfSamples; i++)
+    sum+=analogRead(PCB_SIGNAL_L)+analogRead(PCB_SIGNAL_R);
+
+  sum /= numOfSamples*2;
 }
 
 void loop() {
@@ -32,13 +36,22 @@ void loop() {
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
   
-  Serial.println(sum);
   // put your main code here, to run repeatedly:
-  int s = analogRead(PCB_SIGNAL);
-  Serial.println(s);
-  s = map(s, 0, sum, 255, 0);
-  Serial.println(s);
-  delay(100);
+  int sl = analogRead(PCB_SIGNAL_L);
+  int sr = analogRead(PCB_SIGNAL_R);
+  
+  sr = map(sr, 0, sum, 255, 0);
+  sl = map(sl, 0, sum, 255, 0);
+
+//  Serial.print(sl);
+//  Serial.print(" ");
+//  Serial.println(sr);
+  
+  Serial.print(LHigh(sl));
+  Serial.print(" ");
+  Serial.println(RHigh(sr));
+  
+  delay(80);
 
   analogWrite(PWM1, 255);
   digitalWrite(IN1, HIGH);
