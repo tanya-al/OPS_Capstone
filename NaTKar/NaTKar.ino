d52a144c774edb004e2dbd712c2457ed72eb416c
Tanya Al-Rehani <Tanya Al-Rehani>
2016-05-24 20:21:59 -0700

    a

--- NaTKar/NaTKar.ino
+++ NaTKar/NaTKar.ino
@@ -1,38 +1,49 @@
 #define PCB_SIGNAL_R 14
 #define PCB_SIGNAL_L 15
-#define PWM1 3
+#define PWM1 3    // control the speed of the motors
 #define PWM2 4
-#define IN1 5
-#define IN2 6
+#define IN1 5   // control the motors
+#define IN2 6   //
 #define IN3 7
 #define IN4 8
 
-int suml = 0;
-int sumr = 0;
+double suml = 0;    
+double sumr = 0;
 
 // Declare parameters of the robot to measure and control
-  int leftSpeed = 0;
-  int rightSpeed = 0;
+  double leftSpeed = 0;
+  double rightSpeed = 0;
 
-  int rightRead = 0;
-  int leftRead = 0;
+  double rightRead = 0;
+  double leftRead = 0;
 
-  int rightBaseline = 0;
-  int leftBaseline = 0;
+  double rightBaseline = 0;
+  double leftBaseline = 0;
 
   // Parameters for the controller
-  int rightError = 0;
-  int leftError = 0;
-  int tolerance = 0;
+  double rightError = 0;
+  double leftError = 0;
+  double prevRightError = 0;
+  double prevLeftError = 0;
+  double prevTotalError = 0;
+  double totalError = 0;
+  double tolerance = 0;
+  double defaultSpeed = 120;
+  double derivative = 0; 
+  
 
   int time;
-  double kp;
+  double integral = 0;
+  double kp = 3;
+  double ki = 0; 
+  double kd = 0;
 
 boolean isHigh(int val){ return val<50; }
-//boolean RHigh(int val){ return val<0; }
-int acquireSensor(int pin);
+//int acquireSensor(int pin);
 void move();
+void moveStraight();
 void turnRight();
+void turnLeft();
 void stopMoving();
 
 void setup() {
@@ -55,78 +66,62 @@ void setup() {
   sumr/=1000;
   suml/=1000;
 
-  kp = .25;
+  // is this right?? IDK
+
+  rightBaseline = sumr;
+  leftBaseline = suml;
+  //
+
   time = 10;
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+
+    analogWrite(PWM1, 255);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    delay(3000);
 }
 
 void loop() {
 
-  // put your main code here, to run repeatedly:
-  int sl = analogRead(PCB_SIGNAL_L);
-  int sr = analogRead(PCB_SIGNAL_R);
-
-  sr = map(sr, 0, 1024, 255, 0);
-  sl = map(sl, 0, 1024, 255, 0);
-
-  Serial.print("LEFT: "); Serial.print(sl); Serial.print(" RIGHT: "); Serial.println(sr);
-  Serial.print(isHigh(sl)); Serial.print(" "); Serial.println(isHigh(sr));
-
-  while(sl>95&&sr>95){
-    analogWrite(PWM2, 255);
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, LOW);
-
-    analogWrite(PWM1, 255);
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-  }
-  stopMoving();
-  
-  delay(80);
-
-}
-
-int acquireSensor(int pin){
-  int sum = 0;
-  for(int i = 0; i < 1000; i++){
-    sum+=analogRead(pin);
-  }
-  return sum/1000;
-}
-
-void move(){
+  // Cullen's Whiteboard Code <3
   rightRead = analogRead(PCB_SIGNAL_R);
   leftRead = analogRead(PCB_SIGNAL_L);
+  
   rightError = rightRead - rightBaseline;
   leftError = leftRead - leftBaseline;
+  Serial.println(rightError);
+  Serial.println(leftError);
 
   rightError*=(255/1023);
   leftError*=(255/1023);
 
-  if(rightError > tolerance){
-    rightSpeed -= abs(kp*rightError);
-    leftSpeed += abs(kp*rightError);
-  }
-  if(leftError > tolerance){
-      rightSpeed += abs(Kp*leftError); //shift left
-      leftSpeed -= abs(Kp*leftError);
-  }
-  else{
-      
-  }
-    // Normalize speeds if they exceed the system limit
-    if speed < 0
-      speed = 0
-    if speed > 255
-      speed = 255
+  Serial.println(rightError);
+  Serial.println(leftError);
   
-    
-    // Set motor control signals
-    set motors to forward direction
-       set right PWM (rightSpeed)
-       set left PWM (leftSpeed)
+  totalError = leftError - rightError;
+  Serial.println("Error: ");
+  Serial.println(totalError);
+  
+  derivative = totalError - prevTotalError;
+  integral = totalError + prevTotalError;
+  prevTotalError = totalError;
+  int tspeed;
+  tspeed = kp*totalError + kd*derivative + ki*integral; 
+
+  leftSpeed = defaultSpeed - tspeed;
+  rightSpeed = defaultSpeed + tspeed;
+      
+  // don't need to go backwards ever so just set the parts of the motor that make it go forward to high 
+  // and set the part that makes the motor go backward to low all the time
+
+    analogWrite(PWM2, rightSpeed);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+
+    analogWrite(PWM1, leftSpeed);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+  
 }
 
-void stopMoving(){
-  
-}
